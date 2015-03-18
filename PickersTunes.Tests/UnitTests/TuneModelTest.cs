7607a8f90a6bcd4047c990a36cd006a0ac5b982a
jbrooks036 <joannbrooks036@gmail.com>
2015-03-18 13:35:30 -0500

    TuneModel and TuneRepo tests restored; 9 tests passing

---
+++ PickersTunes.Tests/UnitTests/TuneModelTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PickersTunes.Models;
+namespace TestPickers
+{
+    [TestClass]
+    public class TuneModelTest
+    {
+        /*
+        [TestMethod]
+        public void CreatingATuneStoresItInTunes()
+        {
+        Tune LittleMaggie = new Tune("LittleMaggie");
+        CollectionAssert.Contains(Tune.Tunes, LittleMaggie);
+        }
+        */
+        [TestMethod]
+        public void CreatingATuneStoresItsProperties()
+        {
+            PickersTunes.Models.Tune LittleMaggie = new PickersTunes.Models.Tune("Little Maggie");
+            Assert.AreEqual("Little Maggie", LittleMaggie.Name);
+        }
+    }
+}
